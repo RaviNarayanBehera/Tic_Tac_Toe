f0aad012b3b9b43f44dd82f76ec1d0e351fe615c
RAVINARAYAN <ravinarayan.b9510@gmail.com>
2024-06-03 13:02:02 +0530

    Initial commit

---
+++ lib/Global.dart
@@ -0,0 +1,15 @@
+int drawCondition = 0;
+int player1_0 = 0;
+int player2_X = 0;
+bool playerturn = true;
+List<String> TicTacToeList = [
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+];
